512eebabe1ac0b48d26fc0bdf74798b7d7bbc027
bxl-sx <56504365+bxl-sx@users.noreply.github.com>
2019-10-18 13:18:54 +0800

    Add files via upload
    和上一版本相比较，这次的控制台游戏界面只对变化的位置重新打印，视觉上有很大改善。

--- push box.cpp
+++ push box.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
+#include<stdio.h>
 #include<conio.h>
 #include <windows.h>
 
 using namespace std;
-void Game_Menu();
-void Game_description();
-int DrawMap();
-void Move();
+void Game_Menu(HANDLE hout);
+void Game_description(HANDLE hout);
+void gotoxy(HANDLE hout, int x, int y);
+int DrawMap(HANDLE hout);
+void Move(HANDLE hout);
 int finish();
 void setmap(int n);
 void color(int m);
@@ -14,6 +16,8 @@ bool flag = true;
 int pass = 1;
 #define R 10
 #define C 10
+#define framex 20
+#define framey 14 
 int map[R][C] = {0};
 //关卡1 
 int map1[R][C] = {
@@ -49,27 +53,43 @@ int map3[R][C]={
 { 1,3,3,3,3,3,0,0,1,0 },
 { 1,1,1,1,1,1,1,1,1,0 },
 };
-void Game_Menu(){
+void Game_Menu(HANDLE hout){//游戏开始菜单 
 	system("cls");
-	cout << "*******************" << endl;
-	cout << "      推箱子       " << endl;
-	cout << "    按s或S开始     " << endl;
-	cout << "    按q或Q退出     " << endl;
-	cout << "*******************" << endl;
+	gotoxy(hout, framex, 1);
+	cout << "*******************";
+	gotoxy(hout, framex, 3);
+	cout << "      推箱子       ";
+	gotoxy(hout, framex, 5);
+	cout << "    按s或S开始     ";
+	gotoxy(hout, framex, 7);
+	cout << "    按q或Q退出     ";
+	gotoxy(hout, framex, 9);
+	cout << "游戏前关闭中文输入 ";
+	gotoxy(hout, framex, 11);
+	cout << "*******************";
 	_getch();
 };
-void Game_description(){
-	//system("cls");
-	cout << "*****************************\n";
-	cout << "     按方向键上下左右移动    \n";
-	cout << " 所有箱子到达目的地游戏胜利  \n";
-	cout << "*****************************\n";
+void Game_description(HANDLE hout){//操作提示 
+	system("cls");
+	gotoxy(hout, framex, 1);
+	cout << "*****************************";
+	gotoxy(hout, framex, 3);
+	cout << "     按方向键上下左右移动    ";
+	gotoxy(hout, framex, 5);
+	cout << " 所有箱子到达目的地游戏胜利  ";
+	gotoxy(hout, framex, 7);
+	cout << "         按q或Q退出          ";
+	gotoxy(hout, framex, 9);
+	cout << "*****************************";
 };
-int DrawMap(){
-	cout << "第" << pass << "关\n";
-	for(int i = 0; i < R; i++){
-		for(int j = 0; j < C; j++){
-			switch(map[i][j]){
+void gotoxy(HANDLE hout, int x, int y){
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(hout, pos);
+};
+void print(int i){//根据map值输出相应的图形 
+	switch(i){
 				case 0:
 					color(0x7);
 					cout << "  ";  //空地■★□?◇◆??¤
@@ -101,12 +121,23 @@ int DrawMap(){
    				default:
     				break;
    			}
+}
+int DrawMap(HANDLE hout){//新的关卡开始时载入地图 
+	//HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
+	gotoxy(hout, framex + C, framey - 3);
+	color(0xF);
+	cout << "第" << pass << "关";
+	//printf("第%d关", pass); 
+	for(int i = 0; i < R; i++){
+		gotoxy(hout, framex, framey + i);
+		for(int j = 0; j < C; j++){
+			
+			print(map[i][j]);
 		}
-		cout << '\n';
 	}
 	return 0;
 };
-void Move(){
+void Move(HANDLE hout){//移动小人 
 	int x = 0, y = 0;
 	for(int i = 0; i < R; i++){
 		for(int j = 0; j < C; j++){
@@ -117,21 +148,35 @@ void Move(){
 			}
 		}
 	}
-	cout << "当前位置:(" << x << ',' << y << ")\n";
+	gotoxy(hout, framex, framey + R);
+	color(0xF);
+	printf("当前位置:(%d, %d)", x, y);
 	int ch = _getch();
+	
 	switch(ch){
+		
 		case 'w':
 		case 'W':
 		case 72:
 			if(map[x - 1][y] == 0 || map[x - 1][y] == 3){
 				map[x][y] -= 5;
+				gotoxy(hout, framex + 2 * y, framey + x);
+				print(map[x][y]);
 				map[x - 1][y] += 5;
+				gotoxy(hout, framex + 2 * y, framey + x - 1);
+				print(map[x - 1][y]);
 			}
 			else if(map[x - 1][y] == 4 || map[x - 1][y] == 7){
 				if(map[x - 2][y] == 0 || map[x - 2][y] == 3){
 					map[x][y] -= 5;
+					gotoxy(hout, framex + 2 * y, framey + x);
+					print(map[x][y]);
 					map[x - 1][y] += 1;
+					gotoxy(hout, framex + 2 * y, framey + x - 1);
+					print(map[x - 1][y]);
 					map[x - 2][y] += 4;
+					gotoxy(hout, framex + 2 * y, framey + x - 2);
+					print(map[x - 2][y]);
 				}
 			}
 			break;
@@ -141,13 +186,23 @@ void Move(){
 		case 80:
 			if(map[x + 1][y] == 0 || map[x + 1][y] == 3){
 				map[x][y] -= 5;
+				gotoxy(hout, framex + 2 * y, framey + x);
+				print(map[x][y]);
 				map[x + 1][y] += 5;
+				gotoxy(hout, framex + 2 * y, framey + x + 1);
+				print(map[x + 1][y]);
 			}
 			else if(map[x + 1][y] == 4 || map[x + 1][y] == 7){
 				if(map[x + 2][y] == 0 || map[x + 2][y] == 3){
 					map[x][y] -= 5;
+					gotoxy(hout, framex + 2 * y, framey + x);
+					print(map[x][y]);
 					map[x + 1][y] += 1;
+					gotoxy(hout, framex + 2 * y, framey + x + 1);
+					print(map[x + 1][y]);
 					map[x + 2][y] += 4;
+					gotoxy(hout, framex + 2 * y, framey + x + 2);
+					print(map[x + 2][y]);
 				}
 			}
 			break;
@@ -157,13 +212,23 @@ void Move(){
 		case 75:
 			if(map[x][y - 1] == 0 || map[x][y - 1] == 3){
 				map[x][y] -= 5;
+				gotoxy(hout, framex + 2 * y, framey + x);
+				print(map[x][y]);
 				map[x][y - 1] += 5;
+				gotoxy(hout, framex + 2 * y - 2, framey + x);
+				print(map[x][y - 1]);
 			}
 			else if(map[x][y - 1] == 4 || map[x][y - 1] == 7){
 				if(map[x][y - 2] == 0 || map[x][y - 2] == 3){
 					map[x][y] -= 5;
+					gotoxy(hout, framex + 2 * y, framey + x);
+					print(map[x][y]);
 					map[x][y - 1] += 1;
+					gotoxy(hout, framex + 2 * y - 2, framey + x);
+					print(map[x][y - 1]);
 					map[x][y - 2] += 4;
+					gotoxy(hout, framex + 2 * y - 4, framey + x);
+					print(map[x][y - 2]);
 				}
 			}
 			break;
@@ -173,13 +238,23 @@ void Move(){
 		case 77:
 			if(map[x][y + 1] == 0 || map[x][y + 1] == 3){
 				map[x][y] -= 5;
+				gotoxy(hout, framex + 2 * y, framey + x);
+				print(map[x][y]);
 				map[x][y + 1] += 5;
+				gotoxy(hout, framex + 2 * y + 2, framey + x);
+				print(map[x][y + 1]);
 			}
 			else if(map[x][y + 1] == 4 || map[x][y + 1] == 7){
 				if(map[x][y + 2] == 0 || map[x][y + 2] == 3){
 					map[x][y] -= 5;
+					gotoxy(hout, framex + 2 * y, framey + x);
+					print(map[x][y]);
 					map[x][y + 1] += 1;
+					gotoxy(hout, framex + 2 * y + 2, framey + x);
+					print(map[x][y + 1]);
 					map[x][y + 2] += 4;
+					gotoxy(hout, framex + 2 * y + 4, framey + x);
+					print(map[x][y + 2]);
 				}
 			}
 			break;
@@ -190,7 +265,7 @@ void Move(){
 			break;	
 	}
 };
-int finish(){
+int finish(){//判断游戏结束 
 	for(int i = 0; i < R; i++){
 		for(int j = 0; j < C; j++){
 			if(map[i][j] == 4)return 0;
@@ -211,36 +286,46 @@ void setmap(int n){
 			break;
 	}
 };
-void color(int m){
+void color(int m){//改变输出符号的颜色 
 	HANDLE consolehend;
 	consolehend = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(consolehend, m); 
 };
 int main(){
-	Game_Menu();
+	HANDLE hout = GetStdHandle(STD_OUTPUT_HANDLE);
+	
+	Game_Menu(hout);
 	char ch = getch();
 	setmap(pass);
-	
+	Game_description(hout);
+	DrawMap(hout);
 	if(ch == 'q' || ch == 'Q'){
 		return 0;
 	}
 	while(flag){
-		system("cls");
-		Game_description();
-		DrawMap();
-		//while(flag){
-			Move();
-			//Sleep(100);
-			if(finish()){
-				system("cls");
-				DrawMap();
-				cout << " 恭喜，成功过关！\n";
-				system("pause");
-				pass++;
+		Move(hout);
+		if(finish()){	
+			DrawMap(hout);
+			gotoxy(hout, framex, framey + R);
+			cout << " 恭喜，成功过关！";
+			gotoxy(hout, framex, framey + R + 2);
+			cout << "重玩(R)";
+			ch = getch();
+			system("cls");
+			pass++;
+			if(ch == 'r' || ch == 'R')pass--;
+			if(pass > 3) { 
+				gotoxy(hout, framex, framey);
+				cout << " 您已通过全部关卡！";
+				getch();
+				flag = false;
+			} 
+			else{
 				setmap(pass);
-				
+				Game_description(hout);
+				DrawMap(hout);
 			}
-		
+		}
 	}
 	return 0; 
 }
